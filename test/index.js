48f25a4836d321345339745db45062783cd726e2
Ionică Bizău <bizauionica@yahoo.com>
2015-05-03 16:10:43 +0300

    Added tests

---
+++ test/index.js
@@ -0,0 +1,21 @@
+var FlatColors = require("../lib")
+  , Assert = require("assert")
+  ;
+
+it("should support rgb", function (cb) {
+    var red = FlatColors(255, 0, 0);
+    Assert(red, [231, 76, 60]);
+    cb();
+});
+
+it("should support rbg passed as array", function (cb) {
+    var red = FlatColors([255, 0, 0]);
+    Assert(red, [231, 76, 60]);
+    cb();
+});
+
+it("should support hex input", function (cb) {
+    var red = FlatColors("#f00");
+    Assert(red, [231, 76, 60]);
+    cb();
+});
