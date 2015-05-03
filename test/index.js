08e9e9fbcb04ce406c39709556e2297f262b77bf
Ionică Bizău <bizauionica@yahoo.com>
2015-05-03 16:14:19 +0300

    JSDoc comments

--- test/index.js
+++ test/index.js
@@ -1,19 +1,23 @@
+// Dependencies
 var FlatColors = require("../lib")
   , Assert = require("assert")
   ;
 
+// rgb
 it("should support rgb", function (cb) {
     var red = FlatColors(255, 0, 0);
     Assert(red, [231, 76, 60]);
     cb();
 });
 
+// rgb array
 it("should support rbg passed as array", function (cb) {
     var red = FlatColors([255, 0, 0]);
     Assert(red, [231, 76, 60]);
     cb();
 });
 
+// hex
 it("should support hex input", function (cb) {
     var red = FlatColors("#f00");
     Assert(red, [231, 76, 60]);
